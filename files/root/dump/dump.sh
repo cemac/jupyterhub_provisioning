d867148eaed7b91b86bc96cb7f4869cd2aaa8111
Richard Rigby <r.rigby@leeds.ac.uk>
2021-01-20 13:52:03 +0000

    Add bits for creating backup tar file

---
+++ files/root/dump/dump.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# simple script to dump a path to a compressed tar file
+# e.g.:
+#   dump.sh /path/to/dir /path/to/dir.tar.gz
+
+# usage function:
+usage() {
+  echo "$(basename ${0}) source_path out_file"
+}
+
+# error function:
+error() {
+  echo "* ${1}"
+  echo "END : $(date)"
+  exit
+}
+
+# start message:
+echo "START : $(date)"
+
+# check number of arguments:
+if [ "${#}" != "2" ] ; then
+  usage
+  error "incorrect number of argments"
+fi
+
+# get arguments:
+SRC_PATH="${1}"
+OUT_FILE="${2}"
+
+# check source exists:
+if [ ! -e "${SRC_PATH}" ] ; then
+  error "${SRC_PATH} does not appear to exist"
+fi
+
+# directory containing source directory:
+SRC_DIR="$(dirname "${SRC_PATH}")"
+# source file / directory:
+SRC_NAME="$(basename "${SRC_PATH}")"
+# directory which will contain output file:
+OUT_DIR="$(dirname ${OUT_FILE})"
+
+# make output directory:
+mkdir -p "${OUT_DIR}" || error "failed to create directory ${OUT_DIR}"
+# change to working dir:
+cd "${SRC_DIR}" || error "failed to change to directory ${SRC_DIR}"
+# create tar file:
+tar czf "${OUT_FILE}" "${SRC_NAME}"
+
+# end message:
+echo "END : $(date)"
