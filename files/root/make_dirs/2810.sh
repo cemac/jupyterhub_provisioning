196e52b4ce36c2dee7ffb99a3702517491db2dbf
Richard Rigby <r.rigby@leeds.ac.uk>
2024-12-16 10:08:11 +0000

    Add 2810 make_dirs script

---
+++ files/root/make_dirs/2810.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Directories to make:
+MAKE_DIRS='semester_1_coursework_submission'
+
+# Get course users:
+COURSE_GROUP='jh_users_soee2810'
+COURSE_USERS=$(getent group ${COURSE_GROUP} | \
+                 awk -F ':' '{print $NF}' | \
+                 tr ',' '\n' | \
+                 egrep -v '^(earhbu|earjacr|earmgr|lmkk419|snnn998|sgtc615)$')
+
+# Loop through directories to make:
+for MAKE_DIR in ${MAKE_DIRS}
+do
+  # Loop through users:
+  for COURSE_USER in ${COURSE_USERS}
+  do
+    # Create directory, if it does not exist:
+    if [ ! -e "/home/${COURSE_USER}/${MAKE_DIR}" ] ; then
+      mkdir -p /home/${COURSE_USER}/${MAKE_DIR}
+      chown ${COURSE_USER}:${COURSE_USER} /home/${COURSE_USER}/${MAKE_DIR}
+    fi
+  done
+done
