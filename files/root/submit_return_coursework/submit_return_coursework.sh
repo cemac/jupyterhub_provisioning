572a514c81c2e504fbddc632db32b2f4faefec3f
Richard Rigby <r.rigby@leeds.ac.uk>
2026-01-05 11:15:02 +0000

    Update coursework submit / return
    Handle multiple file types ...

--- files/root/submit_return_coursework/submit_return_coursework.sh
+++ files/root/submit_return_coursework/submit_return_coursework.sh
@@ -21,8 +21,8 @@ MARKER_DIR="${5}"
 # Start message:
 echo "START: $(date)"
 
-# File extension (including '.') to look for. All other files will be ignored:
-FILE_EXT='.ipynb'
+# File extensions (including '.') to look for. All other files will be ignored:
+FILE_EXTS='.ipynb .pdf'
 
 # Name of directory within the SUBMIT_DIR to which submitted files will be
 # moved:
@@ -54,53 +54,57 @@ do
   USER_HOME=$(getent passwd ${USER} | awk -F ':' '{print $6}')
   # Check if SUBMIT_DIR exists:
   if [ -d "${USER_HOME}/${SUBMIT_DIR}" ] ; then
-    # Find files in SUBMIT_DIR:
-    find ${USER_HOME}/${SUBMIT_DIR} -maxdepth 1 -type f -name "*${FILE_EXT}" | \
-      while read USER_FILE
+    # For each file extension:
+    for FILE_EXT in ${FILE_EXTS}
     do
-      # Current date in seconds and %Y%m%d_%H%M%S:
-      CURRENT_TIME=$(date +%s)
-      CURRENT_DATE=$(date +%Y%m%d_%H%M%S)
-      # Get file modification date:
-      FILE_TIME=$(stat -c %Y "${USER_FILE}")
-      FILE_AGE=$((${CURRENT_TIME} - ${FILE_TIME}))
-      # If file age is greater than FILE_MIN_AGE:
-      if [ ${FILE_AGE} -gt ${FILE_MIN_AGE} ] ; then
-        # Work out new file name:
-        IN_FILE="$(basename "${USER_FILE}")"
-        OUT_FILE="${USER}_${CURRENT_DATE}_${IN_FILE}"
-        # For each marker:
-        for MARKER_USER in ${MARKER_USERS}
-        do
-          # Marker's HOME:
-          MARKER_HOME=$(getent passwd ${MARKER_USER} | awk -F ':' '{print $6}')
-          # Check for / created submitted directory in marker's HOME:
-          if [ ! -d "${MARKER_HOME}/${MARKER_DIR}" ] ; then
-            mkdir "${MARKER_HOME}/${MARKER_DIR}"
-            if [ "${?}" != 0 ] ; then
-              echo "* failed to create directory ${MARKER_HOME}/${MARKER_DIR}."
-              echo "* exiting ..."
-              exit
+      # Find files in SUBMIT_DIR:
+      find ${USER_HOME}/${SUBMIT_DIR} -maxdepth 1 -type f -name "*${FILE_EXT}" | \
+        while read USER_FILE
+      do
+        # Current date in seconds and %Y%m%d_%H%M%S:
+        CURRENT_TIME=$(date +%s)
+        CURRENT_DATE=$(date +%Y%m%d_%H%M%S)
+        # Get file modification date:
+        FILE_TIME=$(stat -c %Y "${USER_FILE}")
+        FILE_AGE=$((${CURRENT_TIME} - ${FILE_TIME}))
+        # If file age is greater than FILE_MIN_AGE:
+        if [ ${FILE_AGE} -gt ${FILE_MIN_AGE} ] ; then
+          # Work out new file name:
+          IN_FILE="$(basename "${USER_FILE}")"
+          OUT_FILE="${USER}_${CURRENT_DATE}_${IN_FILE}"
+          # For each marker:
+          for MARKER_USER in ${MARKER_USERS}
+          do
+            # Marker's HOME:
+            MARKER_HOME=$(getent passwd ${MARKER_USER} | awk -F ':' '{print $6}')
+            # Check for / created submitted directory in marker's HOME:
+            if [ ! -d "${MARKER_HOME}/${MARKER_DIR}" ] ; then
+              mkdir "${MARKER_HOME}/${MARKER_DIR}"
+              if [ "${?}" != 0 ] ; then
+                echo "* failed to create directory ${MARKER_HOME}/${MARKER_DIR}."
+                echo "* exiting ..."
+                exit
+              fi
+              chown ${MARKER_USER}:${MARKER_GROUP} \
+                "${MARKER_HOME}/${MARKER_DIR}"
             fi
+            cp -p "${USER_FILE}" "${MARKER_HOME}/${MARKER_DIR}/${OUT_FILE}"
             chown ${MARKER_USER}:${MARKER_GROUP} \
-              "${MARKER_HOME}/${MARKER_DIR}"
+              "${MARKER_HOME}/${MARKER_DIR}/${OUT_FILE}"
+            chmod g+w "${MARKER_HOME}/${MARKER_DIR}/${OUT_FILE}"
+          done
+          # Check for / created submitted directory in user's directory:
+          if [ ! -d "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}" ] ; then
+            mkdir "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}"
+            chown ${USER}:${USER_GROUP} \
+              "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}"
           fi
-          cp -p "${USER_FILE}" "${MARKER_HOME}/${MARKER_DIR}/${OUT_FILE}"
-          chown ${MARKER_USER}:${MARKER_GROUP} \
-            "${MARKER_HOME}/${MARKER_DIR}/${OUT_FILE}"
-          chmod g+w "${MARKER_HOME}/${MARKER_DIR}/${OUT_FILE}"
-        done
-        # Check for / created submitted directory in user's directory:
-        if [ ! -d "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}" ] ; then
-          mkdir "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}"
-          chown ${USER}:${USER_GROUP} \
-            "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}"
+          # Move file to submitted directory and make read only:
+          mv "${USER_FILE}" \
+            "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}/${IN_FILE}"
+          chmod 444 "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}/${IN_FILE}"
         fi
-        # Move file to submitted directory and make read only:
-        mv "${USER_FILE}" \
-          "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}/${IN_FILE}"
-        chmod 444 "${USER_HOME}/${SUBMIT_DIR}/${SUBMITTED_DIR}/${IN_FILE}"
-      fi
+      done
     done
   fi
 done
@@ -125,54 +129,58 @@ do
     chown ${MARKER_USER}:${MARKER_GROUP} \
       "${MARKER_HOME}/${MARKER_DIR}/${MARKED_DIR}"
   fi
-  # Check for marked files:
-  find ${MARKER_HOME}/${MARKER_DIR}/${MARKED_DIR} -maxdepth 1 -type f \
-    -name "*${FILE_EXT}" | while read USER_FILE
+  # For each file extension:
+  for FILE_EXT in ${FILE_EXTS}
   do
-    # Remove metadata from file name:
-    IN_FILE="$(basename "${USER_FILE}")"
-    # Check for username in file name:
-    USER=$(echo "${IN_FILE}" | awk -F '_' '{print $1}')
-    USER_GROUP=$(id -gn ${USER})
-    USER_HOME=$(getent passwd ${USER} | awk -F ':' '{print $6}')
-    if [ -z "${USER_HOME}" ] ; then
-      echo "* failed to find user home directory for file ${USER_FILE}"
-      echo "* skipping ..."
-    else
-      # Output file name:
-      OUT_FILE="$(echo "${IN_FILE}" | sed 's|[a-z0-9]\+_[0-9]\+_[0-9]\+_||g')"
-      # Check for / create directory for marked files in user directory:
-      if [ ! -d "${USER_HOME}/${SUBMIT_DIR}" ] ; then
-        mkdir "${USER_HOME}/${SUBMIT_DIR}"
-        chown ${USER}:${USER_GROUP} "${USER_HOME}/${SUBMIT_DIR}"
-      fi
-      if [ ! -d "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}" ] ; then
-        mkdir "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}"
+    # Check for marked files:
+    find ${MARKER_HOME}/${MARKER_DIR}/${MARKED_DIR} -maxdepth 1 -type f \
+      -name "*${FILE_EXT}" | while read USER_FILE
+    do
+      # Remove metadata from file name:
+      IN_FILE="$(basename "${USER_FILE}")"
+      # Check for username in file name:
+      USER=$(echo "${IN_FILE}" | awk -F '_' '{print $1}')
+      USER_GROUP=$(id -gn ${USER})
+      USER_HOME=$(getent passwd ${USER} | awk -F ':' '{print $6}')
+      if [ -z "${USER_HOME}" ] ; then
+        echo "* failed to find user home directory for file ${USER_FILE}"
+        echo "* skipping ..."
+      else
+        # Output file name:
+        OUT_FILE="$(echo "${IN_FILE}" | sed 's|[a-z0-9]\+_[0-9]\+_[0-9]\+_||g')"
+        # Check for / create directory for marked files in user directory:
+        if [ ! -d "${USER_HOME}/${SUBMIT_DIR}" ] ; then
+          mkdir "${USER_HOME}/${SUBMIT_DIR}"
+          chown ${USER}:${USER_GROUP} "${USER_HOME}/${SUBMIT_DIR}"
+        fi
+        if [ ! -d "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}" ] ; then
+          mkdir "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}"
+          chown ${USER}:${USER_GROUP} \
+            "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}"
+        fi
+        # Check if output / returned file already exists ... :
+        if [ -e "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}" ] ; then
+          # Add current time stamp to returned file name:
+          CURRENT_DATE=$(date +%Y%m%d%H%M%S)
+          OUT_FILE="${OUT_FILE/${FILE_EXT}/}_${CURRENT_DATE}${FILE_EXT}"
+        fi
+        # Copy file back to USER dir:
+        cp -p "${USER_FILE}" \
+          "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}"
         chown ${USER}:${USER_GROUP} \
-          "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}"
+          "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}"
+        # Make returned file read only:
+        chmod 444  "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}"
+        # Check for / create returned directory in marker's directory:
+        if [ ! -d "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}" ] ; then
+          mkdir "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}"
+          chown ${MARKER_USER}:${MARKER_GROUP} \
+            "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}"
+        fi
+        # Move file to DONE_DIR:
+        mv "${USER_FILE}" "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}/"
       fi
-      # Check if output / returned file already exists ... :
-      if [ -e "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}" ] ; then
-        # Add current time stamp to returned file name:
-        CURRENT_DATE=$(date +%Y%m%d%H%M%S)
-        OUT_FILE="${OUT_FILE/${FILE_EXT}/}_${CURRENT_DATE}${FILE_EXT}"
-      fi 
-      # Copy file back to USER dir:
-      cp -p "${USER_FILE}" \
-        "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}"
-      chown ${USER}:${USER_GROUP} \
-        "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}"
-      # Make returned file read only:
-      chmod 444  "${USER_HOME}/${SUBMIT_DIR}/${RETURNED_DIR}/${OUT_FILE}"
-      # Check for / create returned directory in marker's directory:
-      if [ ! -d "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}" ] ; then
-        mkdir "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}"
-        chown ${MARKER_USER}:${MARKER_GROUP} \
-          "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}"
-      fi
-      # Move file to DONE_DIR:
-      mv "${USER_FILE}" "${MARKER_HOME}/${MARKER_DIR}/${DONE_DIR}/"
-    fi
+    done
   done
 done
 
